ca9aec1ee7bfa42968c9dedb718f70a1493aad64
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-18 13:34:14 +0300

    planet, star

--- solar_input.py
+++ solar_input.py
@@ -47,15 +47,14 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-	A = line.split(' ')
-	star.R = int(A[1])
-	star.color = A[2]
-	star.m = int(A[3])
-	star.x = int(A[4])
-	star.y = int(A[5])
-	star.Vx = int(A[6])
-	star.Vy = int(A[7])
-    return star  # FIXME: not done yet
+    A = line.split(sep=' ')
+    star.R = int(A[1])
+    star.color = A[2]
+    star.m = float(A[3])
+    star.x = float(A[4])
+    star.y = float(A[5])
+    star.Vx = float(A[6])
+    star.Vy = float(A[7])
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -72,16 +71,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-	A = line.split(' ')
-	planet.R = int(A[1])
-	planet.color = A[2]
-	planet.m = int(A[3])
-	planet.x = int(A[4])
-	planet.y = int(A[5])
-	planet.Vx = int(A[6])
-	planet.Vy = int(A[7])
-    return planet  # FIXME: not done yet
-
+    A = line.split(sep=' ')
+    planet.R = int(A[1])
+    planet.color = A[2]
+    planet.m = float(A[3])
+    planet.x = float(A[4])
+    planet.y = float(A[5])
+    planet.Vx = float(A[6])
+    planet.Vy = float(A[7])
 
 def write_space_objects_data_to_file(output_filename, space_objects):
     """Сохраняет данные о космических объектах в файл.
